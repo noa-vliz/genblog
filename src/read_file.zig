b6bbf110c82ff3d0b808a7a7f5dc17ec07b09147
Noa <chocolatey.noa@gmail.com>
2025-05-09 23:41:22 +0900

    Update read_file.zig

--- src/read_file.zig
+++ src/read_file.zig
@@ -1,20 +1,6 @@
 const std = @import("std");
 
 /// Please free after use
-/// Usage:
-/// ```zig
-/// const read = @import("./read_file.zig");
-/// const std = @import("std");
-///
-/// fn main(){
-///     const content = read.read_file("./test.txt");
-///     const allocation = std.
-///     for (content.items) |line| {
-///         std.debug.print("{s}\n", .{line});
-///         allocation.free(line);
-///     }
-/// }
-/// ```
 ///
 pub fn read_file(path: []const u8) ![]u8 {
     const stderr = std.io.getStdErr().writer();
