54b9dff2c520cb65812ab5f0c20bd09f1e68250c
noa-vliz <chocolatey.noa@gmail.com>
2025-05-10 17:26:05 +0900

    version 1.1.0

--- src/gen_html.zig
+++ src/gen_html.zig
@@ -1,23 +1,54 @@
 const std = @import("std");
 const parser_import = @import("./parse.zig");
 
-const template = @embedFile("./template.html");
+const default_template = @embedFile("./template.html");
 
-pub fn gen_html(info: parser_import.Info) ![]u8 {
+pub fn gen_html(info: parser_import.Info, template: ?[]const u8) ![]u8 {
+    const stderr = std.io.getStdErr().writer();
+    const last_template = template orelse default_template;
     const alc = std.heap.page_allocator;
 
-    const replaced_title = try replace_tag(alc, template, "{TITLE}", info.title);
-    defer alc.free(replaced_title);
+    std.debug.print("Compiling template HTML\n", .{});
 
-    const replaced_date = try replace_tag(alc, replaced_title, "{DATE}", info.date);
-    defer alc.free(replaced_date);
+    const replacements = [_]struct { tag: []const u8, value: []const u8 }{
+        .{ .tag = "{TITLE}", .value = info.title },
+        .{ .tag = "{DATE}", .value = info.date },
+        .{ .tag = "{GENHTML}", .value = info.body },
+    };
 
-    const replaced_body = try replace_tag(alc, replaced_date, "{GENHTML}", info.body);
+    var current_template = try alc.dupe(u8, last_template);
+    errdefer alc.free(current_template);
 
-    return replaced_body;
+    for (replacements) |replacement| {
+        const replaced = replace_tag(alc, current_template, replacement.tag, replacement.value) catch |err| {
+            if (err == Err.TagNotFound) {
+                try stderr.print("| \x1b[33mwarning:\x1b[0m {s} section not found\n", .{replacement.tag});
+                try stderr.print("| {s} not found. Don't need it?\x1b[0m\n", .{replacement.tag});
+                continue;
+            } else {
+                try stderr.print("| \x1b[31mError:\x1b[0m FAILED: {s}\n", .{@errorName(err)});
+                try stderr.print("| This is an error that should not exist\n", .{});
+                std.process.exit(1);
+            }
+        };
+
+        std.debug.print("| {s}\n", .{replacement.tag});
+        alc.free(current_template);
+        current_template = replaced;
+    }
+
+    return current_template;
 }
+const Err = error{
+    TagNotFound,
+    OutOfMemory,
+};
 
-fn replace_tag(allocator: std.mem.Allocator, source: []const u8, tag: []const u8, value: []const u8) ![]u8 {
+fn replace_tag(allocator: std.mem.Allocator, source: []const u8, tag: []const u8, value: []const u8) Err![]u8 {
+    if (std.mem.indexOf(u8, source, tag) == null) {
+        return Err.TagNotFound;
+    }
+
     const size = std.mem.replacementSize(u8, source, tag, value);
     const buffer = try allocator.alloc(u8, size);
     _ = std.mem.replace(u8, source, tag, value, buffer);
--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,22 @@ const read_file = @import("read_file.zig");
 const template = @embedFile("./template_doc");
 const version_info = @embedFile("genblog_version");
 
-fn version() !void {
+const Command = enum {
+    generate,
+    create_template,
+    use_template,
+    show_version,
+    show_help,
+};
+
+const Options = struct {
+    command: Command,
+    input_file: ?[]const u8,
+    template_file: ?[]const u8,
+    output_file: ?[]const u8,
+};
+
+fn print_version() !void {
     const v = try util.trim(version_info);
     std.debug.print("genblog {s}\n", .{v});
     std.debug.print("Copyright (C) 2025 noa-vliz.\n", .{});
@@ -15,64 +30,141 @@ fn version() !void {
     std.debug.print("Source: https://github.com/noa-vliz/genblog\n", .{});
 }
 
+fn print_usage(program_name: []const u8) !void {
+    const stderr = std.io.getStdErr().writer();
+    try stderr.print("Usage:\n", .{});
+    try stderr.print("  {s} <file>                    Convert Markdown to HTML\n", .{program_name});
+    try stderr.print("  {s} -t, --template <file>     Create template file\n", .{program_name});
+    try stderr.print("  {s} -w, --with-template <template> <file>  Use custom template\n", .{program_name});
+    try stderr.print("  {s} -v, --version             Show version information\n", .{program_name});
+    try stderr.print("  {s} -h, --help                Show this help message\n", .{program_name});
+}
+
+fn parse_args(args: []const []const u8) !Options {
+    const stderr = std.io.getStdErr().writer();
+
+    if (args.len < 2) {
+        try stderr.print("{s}: No file specified.\n", .{args[0]});
+        try stderr.print("For usage instructions, run {s} -h or --help\n", .{args[0]});
+        std.process.exit(1);
+    }
+
+    // デフォルトオプション
+    var options = Options{
+        .command = .generate,
+        .input_file = null,
+        .template_file = null,
+        .output_file = null,
+    };
+
+    // 最初の引数をチェック
+    const first_arg = args[1];
+
+    // テンプレート作成オプション
+    if (std.mem.eql(u8, first_arg, "--template") or std.mem.eql(u8, first_arg, "-t")) {
+        options.command = .create_template;
+        if (args.len < 3) {
+            try stderr.print("Please specify a file for template creation\n", .{});
+            try print_usage(args[0]);
+            std.process.exit(1);
+        }
+        options.output_file = args[2];
+        return options;
+    }
+
+    // カスタムテンプレート使用オプション
+    if (std.mem.eql(u8, first_arg, "--with-template") or std.mem.eql(u8, first_arg, "-w")) {
+        options.command = .use_template;
+        if (args.len < 4) {
+            try stderr.print("{s}: Not enough arguments for template usage\n", .{args[0]});
+            try print_usage(args[0]);
+            std.process.exit(1);
+        }
+        options.template_file = args[2];
+        options.input_file = args[3];
+        return options;
+    }
+
+    // バージョン表示オプション
+    if (std.mem.eql(u8, first_arg, "--version") or std.mem.eql(u8, first_arg, "-v")) {
+        options.command = .show_version;
+        return options;
+    }
+
+    // ヘルプ表示オプション
+    if (std.mem.eql(u8, first_arg, "--help") or std.mem.eql(u8, first_arg, "-h")) {
+        options.command = .show_help;
+        return options;
+    }
+
+    // デフォルトは入力ファイルとして扱う
+    options.input_file = first_arg;
+    return options;
+}
+
+fn create_template_file(file_path: []const u8) !void {
+    const file = try std.fs.cwd().createFile(file_path, .{});
+    defer file.close();
+
+    try file.writeAll(template);
+    std.debug.print("Template created to {s}\n", .{file_path});
+}
+
+fn generate_html_file(alc: std.mem.Allocator, input_file: []const u8, custom_template: ?[]const u8) !void {
+    const output_file = try std.fmt.allocPrint(alc, "{s}.html", .{input_file});
+    defer alc.free(output_file);
+
+    const parsed_info = try parse.parse_file(input_file);
+    std.debug.print("title: {s}\ndate: {s}\n", .{ parsed_info.title, parsed_info.date });
+
+    const generated_html = try gen_html.gen_html(parsed_info, custom_template);
+    defer alc.free(generated_html);
+
+    const file = try std.fs.cwd().createFile(output_file, .{});
+    defer file.close();
+
+    try file.writeAll(generated_html);
+    std.debug.print("HTML file created to {s}\n", .{output_file});
+}
+
 pub fn main() !void {
     const alc = std.heap.page_allocator;
 
-    const stderr = std.io.getStdErr().writer();
-
     const args = std.process.argsAlloc(alc) catch |err| {
+        const stderr = std.io.getStdErr().writer();
         try stderr.print("Failed to get args: {s}\n", .{@errorName(err)});
         std.process.exit(1);
     };
+    defer std.process.argsFree(alc, args);
 
-    if (args.len == 1) {
-        try stderr.print("{s}: No file specified.\n", .{args[0]});
-        try stderr.print("Usage: {s} [-tv] <file>\n", .{args[0]});
-        std.process.exit(1);
-    }
+    // コマンドライン引数を解析
+    const options = try parse_args(args);
 
-    // template生成フラグ
-    const is_template = std.mem.eql(u8, args[1], "--template");
-    const is_template2 = std.mem.eql(u8, args[1], "-t");
-
-    if (is_template or is_template2) {
-        if (args.len == 3) {
-            const file = try std.fs.cwd().createFile(args[2], .{});
-            defer file.close();
-
-            try file.writeAll(template);
-            std.debug.print("Template created to {s}\n", .{args[2]});
-            std.process.exit(0);
-        } else {
-            std.debug.print("Please specify a file\n", .{});
-            std.debug.print("Usage: {s} --template <file>\n", .{args[0]});
-            std.process.exit(1);
-        }
-    }
-
-    // version
-    const is_version = std.mem.eql(u8, args[1], "--version");
-    const is_version2 = std.mem.eql(u8, args[1], "-v");
-
-    if (is_version or is_version2) {
-        try version();
-        std.process.exit(0);
-    }
-
-    // ファイルの内容を順序に見て出力する
-    for (args[1..]) |arg| {
-        const output_file = try std.fmt.allocPrint(alc, "{s}.html", .{arg});
-
-        const tmp = try parse.parse_file(arg);
-        std.debug.print("title: {s}\ndate: {s}\n", .{ tmp.title, tmp.date });
-
-        const generated_html = try gen_html.gen_html(tmp);
-
-        const file = try std.fs.cwd().createFile(output_file, .{});
-
-        try file.writeAll(generated_html);
-        std.debug.print("HTML file created to {s}\n", .{output_file});
-
-        alc.free(generated_html);
+    switch (options.command) {
+        .create_template => {
+            try create_template_file(options.output_file.?);
+        },
+        .use_template => {
+            const template_content = try read_file.read_file(options.template_file.?);
+            try generate_html_file(alc, options.input_file.?, template_content);
+        },
+        .show_version => {
+            try print_version();
+        },
+        .show_help => {
+            try print_usage(args[0]);
+        },
+        .generate => {
+            // 複数ファイルの処理をサポート
+            if (args.len > 2) {
+                // 最初の引数以降のすべてのファイルを処理
+                for (args[1..]) |input_file| {
+                    try generate_html_file(alc, input_file, null);
+                }
+            } else {
+                // 単一ファイルの処理
+                try generate_html_file(alc, options.input_file.?, null);
+            }
+        },
     }
 }
